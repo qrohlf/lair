ce9fddb183f6f316048549bca20bb1d2a62a4e5f
Quinn Rohlf <qr@qrohlf.com>
2014-04-09 09:27:14 -0700

    forgot to uncomment the part of the script that sets the hostname

--- scripts/set-fqdn.sh
+++ scripts/set-fqdn.sh
@@ -2,6 +2,6 @@ hostentry="127.0.0.1 $1 ${1%%.*}"
 if grep --quiet "$hostentry" /etc/hosts; then
     echo "hosts file fqdn configured correctly"
 else
-    # echo "$hostentry" >> /etc/hosts
+    echo "$hostentry" >> /etc/hosts
     echo "added fqdn entry '$hostentry' to host file"
 fi
