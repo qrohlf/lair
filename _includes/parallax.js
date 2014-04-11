bc7d3034b97cc2f6e4a526ce66bfe403795cf8f4
Quinn Rohlf <qr@qrohlf.com>
2014-04-10 19:06:05 -0700

    disable parallax on mobile devices

--- _includes/parallax.js
+++ _includes/parallax.js
@@ -1,10 +1,17 @@
+// detect mobile browsers
+function mobileBrowser() {
+    return (/Android|iPhone|iPad|iPod|BlackBerry/i).test(navigator.userAgent || navigator.vendor || window.opera);
+}
+
 // parallax header
-jQuery(document).ready(function($) {
-    $(window).scroll( function()
-    {
-        var scroll = $(window).scrollTop(), slowScroll = scroll/3;
-        var headerheight = $('header').height();
-        $('header').css({ transform: "translateY(" + slowScroll + "px)" });
-        $('header .container').css({ opacity: Math.min(1, 1.2 - scroll/headerheight) });
+if (!mobileBrowser()) { // parallax scrolling is janky on mobile, so we don't do it.
+    jQuery(document).ready(function($) {
+        $(window).scroll( function()
+        {
+            var scroll = $(window).scrollTop(), slowScroll = scroll/3;
+            var headerheight = $('header').height();
+            $('header').css({ transform: "translateY(" + slowScroll + "px)" });
+            $('header .container').css({ opacity: Math.min(1, 1.2 - scroll/headerheight) });
+        });
     });
-});
+}
